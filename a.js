4329c8ead89e5f6017ffa9e3609266c52fb7f9ba
BA-Baracus <artis.bevalds@gmail.com>
2020-12-08 00:37:45 +0100

    Update a.js

--- a.js
+++ a.js
@@ -13,29 +13,9 @@
 var hasRun = false;
 console.log ("Do you see this?");
 
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
-
-
-var d = new Date();
-var seconds = d.getSeconds()
-var milliseconds = d.getMilliseconds()
-var msLeft = 60 * 1000 - seconds * 1000 - milliseconds;
-
-if (!hasRun) {
-        hasRun = true;
-        unsafeWindow.setTimeout(doSomething,msLeft);
-}
-
-
-function doSomething(){
-   console.log ("=======Preparing countdown timer=========");
-   unsafeWindow.setTimeout(doSomething,15000);
-
 var divs =document.getElementsByClassName("rangeslider");
 divs=Array.from(divs);
-    console.log ("===Time has elapsed, will put the button in=========");
 divs.forEach(function(div){
   div.classList.remove('rangeslider--disabled');
- });
-}
+});
 
